177e6225ec9a8eb142db1cc2a18152955373ebe7
noah <noah@g1project.net>
2016-11-03 14:10:54 +0900

    테스트 커밋 한글 7

--- src/main/java/net/noah/com/controller/MainController.java
+++ src/main/java/net/noah/com/controller/MainController.java
@@ -49,7 +49,7 @@ public class MainController {
 //        JPAQuery jpaQuery = new JPAQuery(entityManager);
 //        List<Member> memberList = jpaQuery.from(member).list(QMember.member);
 //        model.addAttribute("memberList", memberList);
-        //sdsdsddddddasdsaasdasdfsdfssdfsdfsfssdfsdfsdfs
+        //sdsdsddddddasdsaasdasdfsdfssdfsdfsfssdfsdfsdfssdfsdfsfsfs
         return "pages/entity";
     }
 
